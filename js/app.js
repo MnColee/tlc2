ee0f077afa1a8d81bc0e48dd1ab115729df893f4
Colee Abrematia <cabrematia@yahoo.com>
2022-05-20 21:53:59 +0800

    multiple dropdown

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 //navbar shrink funtion
 $(function () {
-    $(window).scroll(function (){
-        if ($(window).scrollTop() <= 40) {
-            $('.navbar').removeClass('scroll_navbar')
-            $('.logo img').removeClass('scroll_logo')
-            $('.nav-item a').removeClass('scroll_nav-link')
-            }else{
-                $('.navbar').addClass('scroll_navbar')
-                $('.logo img').addClass('scroll_logo')
-                $('.nav-item a').addClass('scroll_nav-link')
-            }
-        })
-    })
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= 40) {
+      $('.navbar').removeClass('scroll_navbar')
+      $('.logo img').removeClass('scroll_logo')
+      $('.nav-item a').removeClass('scroll_nav-link')
+    } else {
+      $('.navbar').addClass('scroll_navbar')
+      $('.logo img').addClass('scroll_logo')
+      $('.nav-item a').addClass('scroll_nav-link')
+    }
+  })
+})
 
 //carousel funtion
-$(function(){
-    $('.carousel').carousel({
-      interval: 5000,
-      pause: false
-    }) ;
-    }) ;
+$(function () {
+  $('.carousel').carousel({
+    interval: 5000,
+    pause: false,
+  })
+})
